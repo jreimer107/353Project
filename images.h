646e50a8ff1790b6093257a6b6cbd165965dffde
Luke Leo Richmond <lrichmond2@tux-36.cae.wisc.edu>
2018-04-25 08:22:45 -0500

    Updated Images.c and images.h

--- images.h
+++ images.h
@@ -4,6 +4,26 @@
 #include <stdint.h>
 #include <stdio.h>
 
+<<<<<<< HEAD
+// Bitmap sizes for bat
+#define BAT_WIDTH 40
+#define BAT_HEIGHT 22
+//Bitmap sizes for Steve
+#define STEVE_WIDTH 22
+#define STEVE_HEIGHT 42
+//Bitmap size for tear
+#define TEAR_WIDTH 10
+#define TEAR_HEIGHT 10
+//Bitmap size for zombie
+#define ZOMBIE_WIDTH 24
+#define ZOMBIE_HEIGHT 46
+
+
+#define       PLANE_HEIGHT 	58	// height of plane image in pixels
+#define       PLANE_WIDTH  	47	// width of plane image in pixels
+#define				MISSLE_HEIGHT	11	// height of missle image in pixels
+#define				MISSLE_WIDTH	7		// width of missle image in pixels
+=======
 //Change these as needed for bitmaps
 #define     HERO_HEIGHT 	24	// height of plane image in pixels
 #define     HERO_WIDTH  	16	// width of plane image in pixels
@@ -13,22 +33,15 @@
 #define		BAT_WIDTH		16
 #define		ZOMBIE_HEIGHT	24
 #define		ZOMBIE_WIDTH	16
-#define		BLOB_HEIGHT		16
-#define		BLOB_WIDTH		16
-#define		MIMIC_HEIGHT	24
-#define		MIMIC_WIDTH		16
+>>>>>>> e2e82bc2c6ebfeaaa292efdd85e8f850ad277a8e
 
 
 extern const uint8_t heroBitmap[];
 extern const uint8_t missleBitmap[];
 extern const uint8_t missleErase[];
-extern const uint8_t zombieBitmap[];
-extern const uint8_t zombieErase[];
 extern const uint8_t batBitmap[];
-extern const uint8_t batErase[];
-extern const uint8_t blobBitmap[];
-extern const uint8_t blobErase[];
-extern const uint8_t mimicBitmap[];
-extern const uint8_t mimicErase[];
+extern const uint8_t steveBitmap[];
+extern const uint8_t tearBitmap[];
+extern const uint8_t zombieBitmap[];
 
 #endif
