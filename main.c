da78cbb6d402f94650e5670994dc756e3a201025
jreimer107 <jreimer107@gmail.com>
2018-04-30 07:56:10 -0500

    jreimer107

--- main.c
+++ main.c
@@ -1,25 +1,3 @@
-// Copyright (c) 2015-16, Joe Krachey
-// All rights reserved.
-//
-// Redistribution and use in source or binary form, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-// 1. Redistributions in source form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in
-//    the documentation and/or other materials provided with the distribution.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 #include "main.h"
 #include "actors.h"
 #include "eeprom.h"
@@ -37,12 +15,8 @@ char individual_2[] = "Luke Richmond";
 // Global declared next //
 /////////////////////////
 
-//struct missle * m_head = NULL;
-//struct missle * m_tail = NULL;
 extern actor_t* actors;
 actor_t* hero;
-//enemy_t *enemy_head = NULL;
-//actor_t *actor = NULL;
 
 uint16_t ps2_x, ps2_y;
 
@@ -54,7 +28,6 @@ volatile uint8_t button_count = 0;
 TIMER0_Type* gp_timer;
 GPIOA_Type* portf;
 ADC0_Type* myadc;
-//I2C0_Type i2c_base = (I2C0_Type*)I2C1_BASE;
 
 uint8_t buttons_current;
 bool buttons_pressed[4];
@@ -139,7 +112,6 @@ int main(void) {
     gp_timer = (TIMER0_Type*)TIMER0_BASE;
     myadc = (ADC0_Type*)ADC0_BASE;
     portf = (GPIOA_Type*)GPIOF_BASE;
-    //i2c_base = (I2C0_Type*)I2C1_BASE;
     initialize_hardware();
     gp_timer_start_16(TIMER0_BASE, 7, 15, TICKS, TICKS);
 
@@ -158,7 +130,6 @@ int main(void) {
     while (1) {
         if (TimerA_Done) {
             update_red_led();
-            //update_missles();
             TimerA_Done = false;
         }
 
